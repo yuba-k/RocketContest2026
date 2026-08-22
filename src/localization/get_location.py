b2fb9d127282ece83343139457bf336e02a0c1fa
yuba-k <160005925+yuba-k@users.noreply.github.com>
2026-08-22 21:54:09 +0900

    IMUReceiverクラスを追加し、UARTポートとボーレートを設定する機能を実装

---
+++ src/localization/get_location.py
@@ -0,0 +1,22 @@
+import serial
+import time
+
+import constants
+
+class IMUReceiver():
+    def __init__(self) -> None:
+        self._port = constants.UART_PORT
+        self._baudrate = constants.BAUDRATE
+
+    def open(self):
+        self.ser = serial.Serial(self._port, self._baudrate)
+
+    def getData(self):
+        data = self.ser.readline().decode(errors="ignore")
+        if data == "":
+            return None
+        return data.rstrip()
+
+    def close(self):
+        self.ser.close()
+        KeyboardInterrupt

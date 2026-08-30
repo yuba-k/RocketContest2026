74067a74b9cfdacf770e7dca3d20264f7567854a
yuba-k <160005925+yuba-k@users.noreply.github.com>
2026-08-30 11:26:45 +0900

    Encoderクラスの修正: 左右センサーの追加と角度差計算の機能を実装(TCA9548Aの適用/未テスト)

--- src/localization/wheel_encoder.py
+++ src/localization/wheel_encoder.py
@@ -2,27 +2,51 @@ import board
 import math
 from src import constants
 import adafruit_as5600
+import adafruit_tca9548a
 
 class Encoder():
     def __init__(self):
         i2c = board.I2C()
-        self._sensor = adafruit_as5600.AS5600(i2c)
+        self._tca = adafruit_tca9548a.TCA9548A(i2c)
+        self._sensor_left = adafruit_as5600.AS5600(self._tca[0])
+        self._sensor_right = adafruit_as5600.AS5600(self._tca[1])
         self._radius = constants.RADIUS
-        self._prev_angle = 0.0
+        self.reset_base()
+
+    def reset_base(self):
+        self._prev_angle_left, self._prev_angle_right = self._get_angle()
 
     def _get_angle(self):
-        return self._sensor.angle
+        return self._sensor_left.angle, self._sensor_right.angle
 
-    def _diff_angle(self):
-        angle = self._get_angle()
-        delta = angle - self._prev_angle
+    @staticmethod
+    def _angle_diff(current, previous):
+        delta = current - previous
+
         if delta > 180:
-            delta -= 360
+            delta -=360
         elif delta < -180:
             delta += 360
-        self._prev_angle = angle
 
-    def clac_distance(self):
-        delta = self._get_angle()
-        distance = self._radius * math.radians(delta)
-        return distance
+        return delta
+
+    def get_delta_distance(self):
+        angle_left, angle_right = self._get_angle()
+
+        delta_angle_left = self._angle_diff(
+            angle_left,
+            self._prev_angle_left
+        )
+
+        delta_angle_right = self._angle_diff(
+            angle_right,
+            self._prev_angle_right
+        )
+
+        self._prev_angle_left = angle_left
+        self._prev_angle_right = angle_right
+
+        distance_left = self._radius * math.radians(delta_angle_left)
+        distance_right = self._radius * math.radians(delta_angle_right)
+
+        return distance_left, distance_right

31bfe2a2c92cdd22f16e85ee56ec23db05e616c4
yuba-k <160005925+yuba-k@users.noreply.github.com>
2026-09-01 12:27:24 +0900

    IMUReceiverクラスの修正: スレッドによるデータ受信機能を追加し、データ取得メソッドを更新

--- src/localization/get_location.py
+++ src/localization/get_location.py
@@ -1,5 +1,7 @@
 import serial
 import time
+import queue
+import threading
 
 from src import constants
 
@@ -7,17 +9,29 @@ class IMUReceiver():
     def __init__(self) -> None:
         self._port = constants.UART_PORT
         self._baudrate = constants.BAUDRATE
+        self.queue = queue.Queue(maxsize=1)
+        self.stop_event = threading.Event()
 
     def open(self):
         self.ser = serial.Serial(self._port, self._baudrate)
+        self.run = True
+        self.th1 = threading.Thread(target=self.update_loop,daemon=True)
+        self.th1.start()
 
-    def getData(self):
-        data = self.ser.readline().decode(errors="ignore")
-        if data == "":
-            return None
-        return data.rstrip()
+    def update_loop(self):
+        while self.stop_event:
+            try:
+                self.queue.put(self.ser.readline().decode(errors="ignore"))
+            except queue.Full:
+                pass
+            time.sleep(0.01)
+
+    def get_data(self):
+        return self.queue.get()
 
     def close(self):
+        self.stop_event.set()
+        self.th1.join()
         self.ser.close()
 
 if __name__ == "__main__":
@@ -26,7 +40,7 @@ if __name__ == "__main__":
     receiver.open()
     while True:
         try:
-            print(receiver.getData())
+            print(receiver.get_data())
         except Exception as e:
             print(e)
             break

2d1657268e7f225df2dd44defd01ec4d11c2f4fa
yuba-k <160005925+yuba-k@users.noreply.github.com>
2026-09-12 23:42:27 +0900

    自己位置推定値格納のQueueを追加

--- src/localization/get_location.py
+++ src/localization/get_location.py
@@ -2,11 +2,13 @@ import get_imu
 import wheel_encoder
 import math
 import time
+import queue
 
 class Location:
     def __init__(self) -> None:
         self.x, self.y = 0.0, 0.0
         self.latest_yaw = 0.0
+        self.queue = queue.Queue(maxsize=1)
         self.imu = get_imu.IMUReceiver()
         self.imu.open()
         self.encoder = wheel_encoder.Encoder()
@@ -25,5 +27,10 @@ class Location:
         yaw_rad = math.radians(self.latest_yaw)
         self.x += d * math.cos(yaw_rad)
         self.y += d * math.sin(yaw_rad)
+        try:
+            self.queue.get_nowait()
+        except queue.Empty:
+            pass
+        self.queue.put_nowait(f"{self.x},{self.y},{self.latest_yaw}")
         time.sleep(0.02)
 

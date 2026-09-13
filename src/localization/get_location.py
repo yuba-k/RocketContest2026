67aafabea6ef172cc0e78cfad58337e4fc0f7ef8
yuba-k <160005925+yuba-k@users.noreply.github.com>
2026-09-13 23:23:49 +0900

    update_loopのループ化，main関数を追加

--- src/localization/get_location.py
+++ src/localization/get_location.py
@@ -3,6 +3,7 @@ import wheel_encoder
 import math
 import time
 import queue
+import threading
 
 class Location:
     def __init__(self) -> None:
@@ -14,23 +15,37 @@ class Location:
         self.encoder = wheel_encoder.Encoder()
 
     def update_loop(self):
-        raw_line = self.imu.get_data(get_imu.DataMode.FULL)
-        if raw_line != "None" and raw_line != "Empty":
-            tmp = raw_line.split(",")
-            roll = float(tmp[0].split(":")[1])
-            pitch = float(tmp[1].split(":")[1])
-            yaw = float(tmp[2].split(":")[1])
-            degree = float(tmp[3].split(":")[1])
-            self.latest_yaw = yaw
-        d_left, d_right = self.encoder.get_delta_distance()
-        d = (d_left + d_right)/2
-        yaw_rad = math.radians(self.latest_yaw)
-        self.x += d * math.cos(yaw_rad)
-        self.y += d * math.sin(yaw_rad)
+        while True:
+            raw_line = self.imu.get_data(get_imu.DataMode.FULL)
+            if raw_line != "None" and raw_line != "Empty":
+                tmp = raw_line.split(",")
+                roll = float(tmp[0].split(":")[1])
+                pitch = float(tmp[1].split(":")[1])
+                yaw = float(tmp[2].split(":")[1])
+                degree = float(tmp[3].split(":")[1])
+                self.latest_yaw = yaw
+            d_left, d_right = self.encoder.get_delta_distance()
+            d = (d_left + d_right)/2
+            yaw_rad = math.radians(self.latest_yaw)
+            self.x += d * math.cos(yaw_rad)
+            self.y += d * math.sin(yaw_rad)
+            try:
+                self.queue.get_nowait()
+            except queue.Empty:
+                pass
+            self.queue.put_nowait(f"{self.x},{self.y},{self.latest_yaw}")
+            time.sleep(0.02)
+
+def main():
+    locate = Location()
+    th1 = threading.Thread(target=locate.update_loop, daemon=True)
+    th1.start()
+    while True:
         try:
-            self.queue.get_nowait()
+            print(locate.queue.get_nowait())
         except queue.Empty:
             pass
-        self.queue.put_nowait(f"{self.x},{self.y},{self.latest_yaw}")
-        time.sleep(0.02)
+        time.sleep(1)
 
+if __name__ == "__main__":
+    main()
